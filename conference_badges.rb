d4876333dc1380dd759316345a9bca503a4f4e99
Christina Delavar <c.delavar@gmail.com>
2018-09-14 04:23:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.map.each_with_index {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
+  attendees.collect.each_with_index {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
 end
