abeba78bb18694afb427315a62d83495743e9b10
Christina Delavar <c.delavar@gmail.com>
2018-09-14 04:32:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(attendees)
   attendees.collect.each_with_index {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
 end
 
-def printer
+def printer(attendees)
   batch_badge_creator(attendees).each {|message| puts message}
   assign_rooms(attendees).each {|message| puts message}
 end
